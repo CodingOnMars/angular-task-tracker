324366794ec624d5f4deeb93c2d4740e98a392ba
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-09-14 10:30:26 +0300

    Added basic structure, components and styles.

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Build an Angular app',
+    day: 'September 14th at 9:30am',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Send a resume',
+    day: 'September 14th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Go to an interview',
+    day: 'September 15th at 12:30pm',
+    reminder: false,
+  },
+];
