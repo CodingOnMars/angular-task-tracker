2ac2a47e20b0fba4876237d5d8272ca07af6984b
CodingOnMars <15357252+CodingOnMars@users.noreply.github.com>
2021-09-14 11:11:40 +0300

    Created a task service. Setup JSON-server. Setup HTTP client. Minor style tweaks

--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -4,19 +4,19 @@ export const TASKS: Task[] = [
   {
     id: 1,
     text: 'Build an Angular app',
-    day: 'September 14th at 9:30am',
+    day: 'September 14th at 9:30',
     reminder: true,
   },
   {
     id: 2,
     text: 'Send a resume',
-    day: 'September 14th at 1:30pm',
+    day: 'September 14th at 1:30',
     reminder: true,
   },
   {
     id: 3,
     text: 'Go to an interview',
-    day: 'September 15th at 12:30pm',
+    day: 'September 15th at 12:30',
     reminder: false,
   },
 ];
